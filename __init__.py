e610262907dc95d6c3f9a0d3a34e9183dd90f8c2
CXH <StartHua@users.noreply.github.com>
2024-09-21 10:35:32 +0800

    Update __init__.py
    免费key 版本

--- __init__.py
+++ __init__.py
@@ -3,10 +3,12 @@ from .deepLX_CXH import *
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "deepLX_CXH":deepLX_CXH
+    "CXH_DeepLX_Free":CXH_DeepLX_Free,
+    "CXH_DeepLX_translate":CXH_DeepLX_translate
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "deepLX_CXH":"deepLX_translate"
+    "CXH_DeepLX_Free":"CXH_DeepLX_Free",
+    "CXH_DeepLX_translate":"CXH_DeepLX_translate"
 }
